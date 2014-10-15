d6f4ce131270e5f6b4c558829c0d0efde69955cd
b1kjsh <joshuaman32@gmail.com>
2014-10-14 23:38:14 -0600

    Removed @updateURL in favor of @downloadURL

--- master.user.js
+++ master.user.js
@@ -6,7 +6,6 @@
 // @description		Master list of salesforce scripts
 // @copyright  		2014+, Joshua Howard
 // @include     	https://*.salesforce.com/*
-// @updateURL	   	https://github.com/b1kjsh/sf_tools/raw/master/master.user.js
 // @downloadURL	   	https://github.com/b1kjsh/sf_tools/raw/master/master.user.js
 // @require		   	https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js
 // @require		   	https://github.com/b1kjsh/sf_tools/raw/master/UserScripts/SF_color_coding.user.js
