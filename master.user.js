0ad75d78ce1db508eb6710bd02f09f5ef0565723
b1kjsh <joshuaman32@gmail.com>
2014-10-14 22:57:01 -0600

    Testing

--- master.user.js
+++ master.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       		MasterList
 // @namespace  		https://github.com/b1kjsh/sf_tools
-// @version    		0.21
+// @version    		0.22
 // @grant       	none
 // @description		Master list of salesforce scripts
 // @copyright  		2014+, Joshua Howard
