264c8b340269e4914391baae1e1139467c9214a9
b1kjsh <joshuaman32@gmail.com>
2014-10-14 22:34:00 -0600

    Update masterlist

--- master.user.js
+++ master.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       MasterList
 // @namespace  http://use.i.E.your.homepage/
-// @version    0.1
+// @version    0.11
 // @grant       none
 // @description  Master list of salesforce scripts
 // @require    http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js
